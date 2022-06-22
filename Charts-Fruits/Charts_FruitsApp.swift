041716656dd856fb0d5e2c3d2a017ee95f759e59
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-22 18:59:39 -0500

    Initial Commit

---
+++ Charts-Fruits/Charts_FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Charts_FruitsApp.swift
+//  Charts-Fruits
+//
+//  Created by Aldair Cosetito Coral on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Charts_FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
